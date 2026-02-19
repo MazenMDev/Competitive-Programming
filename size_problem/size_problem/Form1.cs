f6a73851bfceebe6285f3c18419412b9027d43ba
Mazen Mahmoud <mazen.m.mahmoud0@gmail.com>
2026-02-19 23:53:32 +0200

    Implement size control using up and down arrows

---
+++ size_problem/size_problem/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace size
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            KeyDown += size_control;
+        }
+
+        private void size_control(object sender, KeyEventArgs e)
+        {
+            int size = 10;
+            if(e.KeyCode == Keys.Up)
+            {
+                this.Width -= size;
+                this.Height -= size;
+            }
+            else if(e.KeyCode == Keys.Down)
+            {
+                this.Width += size;
+                this.Height += size;
+            }
+        }
+    }
+}
